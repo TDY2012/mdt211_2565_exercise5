348f71ed34519c13dd10df1c392da72edc0b3a55
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-09-18 01:34:09 +0700

    feat: Added solutions for question number 4 and 5.

---
+++ no4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int level = int.Parse(Console.ReadLine());
+
+        for(int i=0; i<level-1; i++)
+        {
+            Console.Write(" ");
+        }
+        Console.WriteLine("#");
+        for(int i=1; i<level-1; i++)
+        {
+            for(int j=0; j<level-1-i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("#");
+            for(int j=0; j<2*i-1; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("#");
+        }
+
+        if(level>1)
+        {
+            for(int i=0; i<2*level-1; i++)
+            {
+                Console.Write("#");
+            }
+        }
+    }
+}
---
+++ no5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstPosition, lastPosition;
+        int firstYear, lastYear;
+
+        firstPosition = Console.ReadLine();
+        lastPosition = firstPosition;
+        firstYear = int.Parse(Console.ReadLine());
+        lastYear = firstYear;
+
+        string inputPosition;
+        int inputYear;
+        while(lastYear != 2565)
+        {
+            inputPosition = Console.ReadLine();
+            inputYear = int.Parse(Console.ReadLine());
+
+            if(inputYear < firstYear)
+            {
+                firstYear = inputYear;
+                firstPosition = inputPosition;
+            }
+            else if(inputYear > lastYear)
+            {
+                lastYear = inputYear;
+                lastPosition = inputPosition;
+            }
+        }
+
+        Console.WriteLine(firstPosition);
+        Console.WriteLine(firstYear);
+        Console.WriteLine(lastPosition);
+        Console.WriteLine(lastYear);
+        Console.WriteLine(lastYear - firstYear + 1);
+    }
+}
